77ad3ccce8cdc7582090f11765fa22825bbc4987
wangxin <499123396@qq.com>
2025-07-06 22:14:22 +0800

    feat: 实现 RpcProvider 服务注册、请求分发与响应逻辑

--- src/include/rpcprovider.h
+++ src/include/rpcprovider.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <google/protobuf/message.h>
 #include <google/protobuf/service.h>
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/TcpConnection.h>
+#include <string>
+#include <unordered_map>
 
 // 框架提供的专门发布rpc服务的网络对象类
 class RpcProvider {
@@ -17,8 +20,19 @@ private:
     // 组合 EventLoop
     muduo::net::EventLoop m_eventLoop;
 
+    // service 服务类型信息
+    struct ServiceInfo {
+        ::google::protobuf::Service *m_service; // 保存服务对象
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor*> m_methodMap; // 保存服务方法
+    };
+
+    // 存储注册成功的服务对象和其服务方法的所有信息
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap;
+
     // 新的socket连接回调
     void OnConnection(const muduo::net::TcpConnectionPtr&);
     // 已建立连接用户的读写事件回调
     void OnMessage(const muduo::net::TcpConnectionPtr&, muduo::net::Buffer*, muduo::Timestamp);
+    // Closure的回调操作，用于序列化rpc的响应和网络发送
+    void SendRpcResponse(const muduo::net::TcpConnectionPtr&, google::protobuf::Message*);
 };
